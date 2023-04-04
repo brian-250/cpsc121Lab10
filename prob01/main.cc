a0b9cff7c66ec902800da4622c99ad3df1c75fb3
Brian Milian <106508985+brian-250@users.noreply.github.com>
2023-04-04 22:49:03 +0000

    Complete lab10 prob01

--- prob01/main.cc
+++ prob01/main.cc
@@ -19,7 +19,9 @@ std::shared_ptr<FoodPantry> CreatePantry(const std::string &pantry_name) {
   // 1. Create a shared_ptr containing a FoodPantry with
   //    the given pantry_name, and return the shared_ptr.
   // ======================================================
-  return nullptr;
+  std::shared_ptr<FoodPantry> pantry1 =
+      std::make_shared<FoodPantry>(pantry_name);
+  return pantry1;
 }
 
 int main() {
@@ -30,49 +32,56 @@ int main() {
   // 2. Call CreatePantry, passing in "Tuffy's Pantry"
   //    and assign it to a shared_ptr named tuffys_pantry.
   // ======================================================
+  std::shared_ptr<FoodPantry> tuffys_pantry = CreatePantry("Tuffys Pantry");
 
   // 3. With your lab partner, predict what this prints out.
   //    Then uncomment the line below, and run main to check.
   //    Your prediction:
-  // std::cout << tuffys_pantry.use_count() << std::endl;
+  std::cout << tuffys_pantry.use_count() << std::endl;
 
   // =================== YOUR CODE HERE ===================
   // 4. Assign the Volunteers `jc` and `paul` to the
   //    tuffys_pantry using the AssignToPantry function.
   // ======================================================
+  jc.AssignToPantry(tuffys_pantry);
+  paul.AssignToPantry(tuffys_pantry);
 
   // 5. With your lab partner, predict what this prints out.
   //    Then uncomment the line below, and run main to check.
   //    Your prediction:
-  // std::cout << tuffys_pantry.use_count() << std::endl;
+  std::cout << tuffys_pantry.use_count() << std::endl;
 
   // If you're here, uncomment the two lines of code below!
-  // jc.AssignedPantry()->Donate("Mango", 5);
-  // paul.AssignedPantry()->Donate("Mango", 20);
+  jc.AssignedPantry()->Donate("Mango", 5);
+  paul.AssignedPantry()->Donate("Mango", 20);
 
   // 6. With your lab partner, predict what this prints out.
   //    Look in the foodpantry.h file to see what it does.
   //    Then uncomment the line below, and run main to check.
   //    Your prediction:
-  // tuffys_pantry->DisplayPantryItems();
+  tuffys_pantry->DisplayPantryItems();
 
   // =================== YOUR CODE HERE ===================
   // 7. Call CreatePantry and create a new shared_ptr
   //    representing a different pantry location.
   //    Assign `jc` and `paul` to the new pantry.
   // ======================================================
-
+  std::shared_ptr<FoodPantry> different_pantry =
+      CreatePantry("Different Pantry");
+  jc.AssignToPantry(different_pantry);
+  paul.AssignToPantry(different_pantry);
   // 8. With your lab partner, predict what this prints out.
   //    Then uncomment the line below, and run main to check.
   //    Your prediction:
-  // std::cout << tuffys_pantry.use_count() << std::endl;
+  std::cout << different_pantry.use_count() << std::endl;
 
   // 9. With your lab partner, predict what this prints out.
   //    Then uncomment the lines below, and run main to check.
   //    Your prediction:
-  // tuffys_pantry = nullptr;
-  // std::cout << tuffys_pantry.use_count() << std::endl;
+  tuffys_pantry = nullptr;
+  std::cout << tuffys_pantry.use_count() << std::endl;
 
   // 10. Print out the secret key to confirm that you and
   //     your lab partner have read the README.
+  std::cout << "TUFFY121L" << std::endl;
 }
