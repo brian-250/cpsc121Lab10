61f9682307844487c39dadee091475d8a4331b74
Brian Milian <106508985+brian-250@users.noreply.github.com>
2023-04-18 16:07:16 -0700

    Complete phone implementation file

--- prob02/phone.cc
+++ prob02/phone.cc
@@ -1,25 +1,37 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Brian Milian
+// CPSC 121L-01
+// 2023-04-04
+// brianmilian@csu.fullerton.edu
+// @brian-250
 //
 // Lab 10-2
-// If it is a pair programming lab please specify partner below.
-// Partner: @peteranteater
-
+// Pair Programming Lab
+// Partner: @abledaniel
+//
+// This program creates a network class that connects smart pointer phone
+// objects together to allow for communication within the owners of the phones
+//
 #include "phone.h"
 
-// ========================= YOUR CODE HERE =========================
-// This implementation file (phone.cc) is where you should implement
-// the member functions declared in the header (phone.h), only
-// if you didn't implement them inline within phone.h.
-//
-// Remember to specify the name of the class with :: in this format:
-//     <return type> MyClassName::MyFunction() {
-//        ...
-//     }
-// to tell the compiler that each function belongs to the Phone class.
-// ===================================================================
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
 
+#include "message.h"
+
+Phone::Phone(const std::string& owner) : owner_(owner) {}
+std::string Phone::GetOwner() const { return owner_; }
+std::shared_ptr<Message> Phone::AuthorMessage(const std::string& message) {
+  std::shared_ptr<Message> message1 =
+      std::make_shared<Message>(message, owner_);
+  return message1;
+}
+void Phone::AcceptMessage(std::shared_ptr<Message> message) {
+  messages_.push_back(message);
+}
+void Phone::PrintMessages() {
+  for (auto& index : messages_) {
+    std::cout << index->GetSender() << ": " << index->GetMessage() << "\n";
+  }
+}
